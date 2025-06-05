edcbe499a90fb179e71a02f23e0495df4d85c143
UP240104 <up240104@alumnos.upa.edu.mx>
2025-06-05 10:40:18 -0600

    Archivo dia 1

---
+++ day1_lvl2.py
@@ -0,0 +1,24 @@
+print('Excersises - Day 1')
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+print('Diego')
+print('Verdin')
+print('Mexicongo')
+print('no disfruto este curso')
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Diego','Python','Mexicongo']))
+print(type('Diego'))
+print(type('Verdin'))
+print(type('Mexicongo'))
+
